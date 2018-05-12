a12bab492cd1667e94eb453e4bbb21c3f183e4d7
TheSandDoctor <twitter@markyrosongaming.com>
2018-05-11 20:47:13 -0700

    Update call_home
    Updated call_home to reflect the fact that the status page for the bot is now JSON formatted and more complex than before.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -14,10 +14,12 @@ void print_dict(pybind11::dict dict){
  */
 bool call_home(pybind11::object site, std::string user_name){
     //py::print(py::str(site));
-    pybind11::object page = site.attr("Pages").attr("__getitem__")("User:" + user_name + "/status2");
+    pybind11::object page = site.attr("Pages").attr("__getitem__")("User:" + user_name + "/status");
     pybind11::object json = pybind11::module::import("json");
     auto text = page.attr("text")();
     auto data = json.attr("loads")(text).attr("__getitem__")("run").attr("__getitem__")("div_col");
+    //pybind11::print(pybind11::str(Py_True));
+    //if(pybind11::str(data) == pybind11::str(Py_True)) {
     if(pybind11::str(data).is(pybind11::str(Py_True))) {
         return true;
     }
